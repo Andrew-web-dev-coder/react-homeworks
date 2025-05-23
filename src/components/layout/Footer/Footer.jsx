944afc9c48a886e18e93472ffd58a92afe6da0b5
Andrei Yeudakimau <andrei.yeudakimau@student.ehu.lt>
2025-05-23 13:46:56 +0300

    feat: add solutions

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -1,16 +1,4 @@
-
 import React from "react";
-
-
-import React from "react";
-
-
-import React from "react";
-
-import React, { Component } from "react";
-
-
-
 import "./Footer.css";
 import Logo from "@images/logo/Logo.png";
 import Insta from "@images/icons/Instagram.png";
@@ -18,23 +6,8 @@ import Twitter from "@images/icons/Twitter.png";
 import YouTube from "@images/icons/YouTube.png";
 import { footerLinks } from "@data/FooterLinks.js";
 
-
 const Footer = () => {
   const renderLinks = (category) => {
-
-
-const Footer = () => {
-  const renderLinks = (category) => {
-
-
-const Footer = () => {
-  const renderLinks = (category) => {
-
-export default class Footer extends Component {
-  renderLinks(category) {
-
-
-
     const isTemplate = category === "Template";
 
     return (
@@ -54,7 +27,6 @@ export default class Footer extends Component {
         ))}
       </ul>
     );
-
   };
 
   return (
@@ -125,76 +97,3 @@ export default class Footer extends Component {
 };
 
 export default Footer;
-
-  }
-
-  render() {
-    return (
-      <footer>
-        <div className="container__top">
-          <div className="logo__motto">
-            <a href="#" onClick={(e) => e.preventDefault()}>
-              <img src={Logo} alt="logo" className="logo" />
-            </a>
-            <p className="motto">
-              Takeaway & Delivery template <br />
-              for small – medium businesses.
-            </p>
-          </div>
-
-          <div className="footer-content">
-            <div className="footer-column">
-              <h4 className="footer-column-title">Company</h4>
-              {this.renderLinks("Company")}
-            </div>
-            <div className="footer-column">
-              <h4 className="footer-column-title">Template</h4>
-              {this.renderLinks("Template")}
-            </div>
-            <div className="footer-column">
-              <h4 className="footer-column-title">Flowbase</h4>
-              {this.renderLinks("Flowbase")}
-            </div>
-          </div>
-        </div>
-
-        <div className="footer-hr"></div>
-
-        <div className="footer-bottom">
-          <p className="footer-bottom-text">
-            Built by{" "}
-            <span>
-              <a href="#" onClick={(e) => e.preventDefault()} className="Flowbase">
-                Flowbase
-              </a>
-            </span>{" "}
-            · Powered by{" "}
-            <a href="#" onClick={(e) => e.preventDefault()} className="Webflow">
-              Webflow
-            </a>
-          </p>
-
-          <div className="footer-social-media">
-            <div className="circle">
-              <a href="#" onClick={(e) => e.preventDefault()}>
-                <img src={Insta} alt="Instagram" />
-              </a>
-            </div>
-            <div className="circle">
-              <a href="#" onClick={(e) => e.preventDefault()}>
-                <img src={Twitter} alt="Twitter" />
-              </a>
-            </div>
-            <div className="circle">
-              <a href="#" onClick={(e) => e.preventDefault()}>
-                <img src={YouTube} alt="YouTube" />
-              </a>
-            </div>
-          </div>
-        </div>
-      </footer>
-    );
-  }
-}
-
-
--- src/components/ui/Button/Button.jsx
+++ src/components/ui/Button/Button.jsx
@@ -13,26 +13,4 @@ export function Button({ children, onClick, className = "", ...props }) {
       {children}
     </button>
   );
-
-
-import React, { Component } from "react";
-import "./Button.css";
-
-export class Button extends Component {
-  render() {
-    const { children, onClick, className = "", ...props } = this.props;
-
-    return (
-      <button
-        type="button"
-        onClick={onClick}
-        className={`button ${className}`}
-        {...props}
-      >
-        {children}
-      </button>
-    );
-  }
-
-
 }
