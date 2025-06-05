672049e1743740606451a8709a541c73e5767824
Andrei Yeudakimau <andrei.yeudakimau@student.ehu.lt>
2025-06-05 20:06:30 +0300

    React-HW-8

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { CartProvider } from "@components/Context/Context";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <CartProvider>
+    <Provider store={store}>
       <App />
-    </CartProvider>
+    </Provider>
   </React.StrictMode>
 );
